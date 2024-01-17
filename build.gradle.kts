50251724e7b3a529633bb80ccacca57c80f21e58
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-01-17 23:39:51 +0200

    Add ktor server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     application
     kotlin("jvm")
+    id("io.ktor.plugin") version "2.3.7"
+}
+
+group = "com.nestapp"
+version = "0.0.1"
+description = "NestApp"
+
+application {
+    mainClass.set("com.nestapp.ApplicationKt")
 }
 
 repositories {
@@ -20,37 +29,25 @@ tasks.withType<Test>().configureEach {
 }
 
 dependencies {
-    api("com.xhiteam.dxf:dxf:1.0.0")
-    api("com.github.jchamlin:clipper-java:b4dcd50c51")
-    api("com.google.code.gson:gson:2.10")
-    api("org.uncommons.watchmaker:watchmaker-framework:0.7.1")
-    api("org.apache.xmlgraphics:batik-svg-dom:1.14")
-    api("org.apache.xmlgraphics:batik-swing:1.14")
-    api("org.dom4j:dom4j:2.1.3")
-    api("org.uncommons.watchmaker:watchmaker-swing:0.7.1")
-    api("io.jenetics:jenetics:5.2.0")
+    implementation("io.ktor:ktor-server-core:2.3.7")
+    implementation("io.ktor:ktor-server-netty:2.3.7")
+
+    implementation("com.xhiteam.dxf:dxf:1.0.0")
+
+    implementation("com.github.jchamlin:clipper-java:b4dcd50c51")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("org.uncommons.watchmaker:watchmaker-framework:0.7.1")
+    implementation("org.apache.xmlgraphics:batik-svg-dom:1.14")
+    implementation("org.apache.xmlgraphics:batik-swing:1.14")
+    implementation("org.dom4j:dom4j:2.1.3")
+    implementation("org.uncommons.watchmaker:watchmaker-swing:0.7.1")
+    implementation("io.jenetics:jenetics:5.2.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
     implementation(kotlin("stdlib-jdk8"))
 }
 
-group = "com.yisa.util"
-version = "0.0.1"
-description = "nest4J"
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-tasks.withType<JavaCompile>() {
-    options.encoding = "UTF-8"
-}
-
-tasks.withType<Javadoc>() {
-    options.encoding = "UTF-8"
-}
 kotlin {
     jvmToolchain(8)
 }
---
+++ src/main/java/com/nestapp/ApplicationKt.kt
@@ -0,0 +1,17 @@
+package com.nestapp
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
+}
