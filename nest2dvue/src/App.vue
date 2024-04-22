a7eaa09527c0d57b99f189e628eb3657ba061197
BuJlJlu <golovashchenkom@ukr.net>
2024-04-22 09:01:22 +0300

    update footer

--- nest2dvue/src/App.vue
+++ nest2dvue/src/App.vue
@@ -6,14 +6,14 @@
             <RouterView />
         </div>
 
-        <AppFooter />
+        <Footer />
     </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
 import AppHeader from '@/components/AppHeader.vue';
-import AppFooter from '@/components/AppFooter.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <style scoped>
