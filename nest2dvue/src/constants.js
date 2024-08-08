cbec137c27841a6b68334a3cfb3e1baedeb23f5b
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-08-08 17:30:24 +0300

    fixed some hardcode

--- nest2dvue/src/constants.js
+++ nest2dvue/src/constants.js
@@ -1,3 +1,3 @@
 // src/utils/constants.js
-export const API_URL = 'https://new.nest2d.online/api';
-//export const API_URL = 'http://localhost:8080/api';
+export const API_URL = import.meta.env.VITE_API_URL;
+
